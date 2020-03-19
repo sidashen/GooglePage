18aff05b4eb29f7656de2fd5a3e2d5c48b2452be
sidashen <shashasi8023@163.com>
2020-03-19 23:20:37 +0800

    [shasha] feat: footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import '../styles/footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return(
+  return (
     <footer className="footer">
-    <div className="address">Hong Kong</div>
-    <div className="footer-details footer-display">
-      <ul className="footer-function footer-display">
-        <li>Advertising</li>
-        <li>Business</li>
-        <li>About</li>
-        <li>How Search works</li>
-      </ul>
-      <ul className="footer-info footer-display">
-        <li>Privacy</li>
-        <li>Terms</li>
-        <li>Settings</li>
-      </ul>
-    </div>
-  </footer>
+      <div className="address">Hong Kong</div>
+      <div className="footer-details footer-display">
+        <ul className="footer-function footer-display">
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>Advertising</li>
+          </Link>
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>Business</li>
+          </Link>
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>About</li>
+          </Link>
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>How Search works</li>
+          </Link>
+        </ul>
+        <ul className="footer-info footer-display">
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>Privacy</li>
+          </Link>
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>Terms</li>
+          </Link>
+          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+            <li>Settings</li>
+          </Link>
+        </ul>
+      </div>
+    </footer>
   );
 }
 

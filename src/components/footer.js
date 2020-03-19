f951c9b86f1a60f4f612f757b06a5724f79a7744
sidashen <shashasi8023@163.com>
2020-03-19 23:31:35 +0800

    [shasha] refacotr: footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/footer.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -8,29 +8,29 @@ function Footer() {
       <div className="address">Hong Kong</div>
       <div className="footer-details footer-display">
         <ul className="footer-function footer-display">
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>Advertising</li>
-          </Link>
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          </NavLink>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>Business</li>
-          </Link>
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          </NavLink>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>About</li>
-          </Link>
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          </NavLink>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>How Search works</li>
-          </Link>
+          </NavLink>
         </ul>
         <ul className="footer-info footer-display">
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>Privacy</li>
-          </Link>
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          </NavLink>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>Terms</li>
-          </Link>
-          <Link to='/footer' style={{ color: '#7e7b7b' }}>
+          </NavLink>
+          <NavLink to='/footer' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
             <li>Settings</li>
-          </Link>
+          </NavLink>
         </ul>
       </div>
     </footer>

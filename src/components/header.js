6d2c024c33bba394c4e0b87d880cfe053931665f
sidashen <shashasi8023@163.com>
2020-03-19 23:13:42 +0800

    [shasha] feat: header link

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../icon/iconfont.css';
 import '../styles/header.css';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
     <nav className="header">
-      <div className="link"><a href="#">Gmail</a></div>
-      <div className="link"><a href="#">Images</a></div>
+      <div className="link"><Link to='/header'>Gmail</Link></div>
+      <div className="link"><Link to='/header'>Images</Link></div>
       <i className="iconfont icon-ziyuan"></i>
-      <div className="profile-photo"></div>
+      <Link to='/'><div className="profile-photo"></div></Link>
     </nav>
   );
 }

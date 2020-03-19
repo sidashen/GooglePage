43324edbf991c9142164b18e3eb09af9f9c9f21e
sidashen <shashasi8023@163.com>
2020-03-19 23:53:53 +0800

    [shasha] refacotr: header link

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,21 @@ import '../icon/iconfont.css';
 import '../styles/header.css';
 import { NavLink } from 'react-router-dom';
 
-function Header(props) {
+function Header() {
+  const LinkContents = ['Gmail', 'Images'];
+  const ListItems = LinkContents.map((linkContent) => {
+    <div className="link">
+      <NavLink to='/footer'
+        style={{ color: '#7e7b7b' }}
+        activeStyle={{ color: 'red' }}>
+        {linkContent}
+      </NavLink>
+    </div>
+  });
+
   return (
     <nav className="header">
-      <div className="link">
-        <NavLink to='/header' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
-          Gmail
-          </NavLink>
-      </div>
-      <div className="link">
-        <NavLink to='/header' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
-          Images
-          </NavLink>
-      </div>
+      {ListItems}
       <i className="iconfont icon-ziyuan"></i>
       <NavLink to='/'><div className="profile-photo"></div></NavLink>
     </nav>

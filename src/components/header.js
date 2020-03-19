97e12b1ab1ce8b3c7096ef7fdaae60fc924fa193
sidashen <shashasi8023@163.com>
2020-03-19 23:30:03 +0800

    [shasha] refacotr: header link

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../icon/iconfont.css';
 import '../styles/header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header(props) {
   return (
     <nav className="header">
-      <div className="link"><Link to='/header'>Gmail</Link></div>
-      <div className="link"><Link to='/header'>Images</Link></div>
+      <div className="link">
+        <NavLink to='/header' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
+          Gmail
+          </NavLink>
+      </div>
+      <div className="link">
+        <NavLink to='/header' style={{ color: '#7e7b7b' }} activeStyle={{ color: 'red' }}>
+          Images
+          </NavLink>
+      </div>
       <i className="iconfont icon-ziyuan"></i>
-      <Link to='/'><div className="profile-photo"></div></Link>
+      <NavLink to='/'><div className="profile-photo"></div></NavLink>
     </nav>
   );
 }

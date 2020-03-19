e4afa7bea0d7accd6aaeea15dcf707d4b1a2589c
sidashen <shashasi8023@163.com>
2020-03-19 22:48:02 +0800

    [shasha] feat: header and footer URL

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './styles/App.css';
 import Header from './components/header';
 import Search from './components/search';
 import Footer from './components/footer'
+import HeaderLink from './components/HeaderLink';
+import FooterLink from './components/FooterLink';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Search/>
-      <Footer/>
+      <Header />
+      <Search />
+      <Footer />
+      <Router>
+        <Route path="/header" component={HeaderLink}></Route>
+        <Route path="/footer" component={FooterLink}></Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/HeaderLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeaderLink() {
+  return (
+    <div>
+      <h1>Header Page</h1>
+    </div>
+  );
+}
+
+export default HeaderLink;

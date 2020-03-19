27341628e5fe682277024c04ed2b95f5c7a3a3e2
sidashen <shashasi8023@163.com>
2020-03-19 23:07:31 +0800

    [shasha] refactor: header and footer URL

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './styles/App.css';
-import Header from './components/header';
-import Search from './components/search';
-import Footer from './components/footer'
 import HeaderLink from './components/HeaderLink';
 import FooterLink from './components/FooterLink';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import Search from './components/search';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Search />
-      <Footer />
       <Router>
-        <Route path="/header" component={HeaderLink}></Route>
-        <Route path="/footer" component={FooterLink}></Route>
+        <Header></Header>
+        <switch>
+          <Route path="/" exact component={Search}></Route>
+          <Route path="/header" component={HeaderLink}></Route>
+          <Route path="/footer" component={FooterLink}></Route>
+        </switch>
+        <Footer></Footer>
       </Router>
     </div>
   );
